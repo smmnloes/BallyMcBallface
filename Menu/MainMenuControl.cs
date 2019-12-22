e974328256dc06a85c5d1f1d0ed0eeeb5b1190bb
maximilian <m.loesch@hotmail.de>
2019-12-22 13:41:04 +0100

    set lives to 3 and score to 0 on level start from menu

--- Menu/MainMenuControl.cs
+++ Menu/MainMenuControl.cs
@@ -5,133 +5,149 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using System.IO;
 
-public class MainMenuControl : MonoBehaviour {
+public class MainMenuControl : MonoBehaviour
+{
+    public Dropdown playerDropdown;
+    public Dropdown levelDropdown;
+    public InputField playerInputField;
+
+    public string currentplayer;
+    public int selectedLevel;
+    public Text currentPlayerView;
 
 
-	public Dropdown playerDropdown;
-	public Dropdown levelDropdown;
-	public InputField playerInputField;
+    void Start()
+    {
+        Screen.orientation = ScreenOrientation.Portrait;
+    }
 
-	public string currentplayer;
-	public int selectedLevel;
-	public Text currentPlayerView;
-
-
-	void Start () {
-		
-		Screen.orientation = ScreenOrientation.Portrait;
-
-	}
-
-	void Update () {
-		
-		currentplayer = PlayerStats.playerData.currentPlayer;								//Get current player from PlayerStats and refresh display
-		showCurrentPlayerLevel ();
-
-	}
+    void Update()
+    {
+        currentplayer = PlayerStats.playerData.currentPlayer; //Get current player from PlayerStats and refresh display
+        showCurrentPlayerLevel();
+    }
 
 
-	public void deletePlayer () {
-		if (playerDropdown.options.Count > 0) {												//delete player if list has players
+    public void deletePlayer()
+    {
+        if (playerDropdown.options.Count > 0)
+        {
+            //delete player if list has players
 
-			string playertodelete = playerDropdown.options [playerDropdown.value].text;
+            string playertodelete = playerDropdown.options[playerDropdown.value].text;
 
-			playerDropdown.options.RemoveAt (playerDropdown.value);								//delete from dropdown
+            playerDropdown.options.RemoveAt(playerDropdown.value); //delete from dropdown
 
-			playerDropdown.RefreshShownValue();
-			playerDropdown.value = playerDropdown.options.Capacity;										//show next in list
-			
-			PlayerStats.playerData.players.Remove (playertodelete);	
-			selectedLevel = 1;																			//delete from Dictionary
-		}
-	}
+            playerDropdown.RefreshShownValue();
+            playerDropdown.value = playerDropdown.options.Capacity; //show next in list
+
+            PlayerStats.playerData.players.Remove(playertodelete);
+            selectedLevel = 1; //delete from Dictionary
+        }
+    }
 
 
-	public void LoadByIndex() {																//Start-Button
-		if (currentplayer!="")
-			SceneManager.LoadScene(selectedLevel);
+    public void StartGame()
+    {
+        //Start-Button
+        if (currentplayer != "")
+        {
+            PlayerStats.playerData.currentPlayerLives = 3;
+            PlayerStats.playerData.currentPlayerScore = 0;
+            SceneManager.LoadScene(selectedLevel);
+        }
+    }
 
-	}
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
 
-	public void Quit () {
-
-		#if UNITY_EDITOR 
-		UnityEditor.EditorApplication.isPlaying = false;
-
-		#else 
+#else
 		Application.Quit();
 
-		#endif
+#endif
+    }
 
-	}
-		
-	public void addPlayer () {
-		
-		if (playerInputField.text != "" && !PlayerStats.playerData.players.ContainsKey(playerInputField.text.ToUpper())) {
-			PlayerStats.playerData.players.Add (playerInputField.text.ToUpper(), 1);
-		}																				//add player to Dictionary
-		selectedLevel = 1;
-	}
+    public void addPlayer()
+    {
+        if (playerInputField.text != "" && !PlayerStats.playerData.players.ContainsKey(playerInputField.text.ToUpper()))
+        {
+            PlayerStats.playerData.players.Add(playerInputField.text.ToUpper(), 1);
+        } //add player to Dictionary
 
-	public void acceptPlayerChoice () {													//Back-Button from Playerselect
-		if (playerDropdown.options.Count > 0) {
-			currentplayer = playerDropdown.options [playerDropdown.value].text;			
+        selectedLevel = 1;
+    }
 
-		} else {
-			currentplayer = "";
-		}
-		PlayerStats.playerData.currentPlayer = currentplayer;	
-		selectedLevel = 1;																//Update PlayerStats
-	}
+    public void acceptPlayerChoice()
+    {
+        //Back-Button from Playerselect
+        if (playerDropdown.options.Count > 0)
+        {
+            currentplayer = playerDropdown.options[playerDropdown.value].text;
+        }
+        else
+        {
+            currentplayer = "";
+        }
 
-
-
-	public void showCurrentPlayerLevel (){	
-		if (currentPlayerView != null) {														//show current player and selected level
-			if (currentplayer == "" || currentplayer == null)
-				currentPlayerView.text = "PLEASE SELECT PLAYER";
-			else
-				currentPlayerView.text = "PLAYER: " + currentplayer + "\nLEVEL: " + selectedLevel;
-		}
-	}
-
-	public void populateLevelDropdown () {
-
-			levelDropdown.ClearOptions ();
-
-			if (currentplayer!=""){																	//add possible levels for player to dropdown
-				int levelReached = PlayerStats.playerData.players [currentplayer];
-				for (int i = 1; i <= levelReached; i++) {
-					levelDropdown.options.Add(new Dropdown.OptionData(){text="LEVEL "+(i)});
-				}
-				levelDropdown.RefreshShownValue ();
-
-			} else {
-
-				levelDropdown.options.Add(new Dropdown.OptionData(){text = "CHOOSE PLAYER FIRST!"});					//no player chosen
-				levelDropdown.RefreshShownValue ();
-			}
-			levelDropdown.value = 0;
-
-	}
-
-	public void populatePlayerDropdown () {
-
-		playerDropdown.ClearOptions();
-
-		foreach (string currentKey in PlayerStats.playerData.players.Keys) {						//get all players from Dictionary and put in dropdown
-			playerDropdown.options.Add (new Dropdown.OptionData (){ text = currentKey });
-
-		}
-		playerDropdown.value = playerDropdown.options.Capacity; 											//show last player created
-	
-	}
-
-	public void changeSelectedLevel (){
-		
-		levelDropdown.RefreshShownValue ();
-		selectedLevel = levelDropdown.value+1;
-	}
+        PlayerStats.playerData.currentPlayer = currentplayer;
+        selectedLevel = 1; //Update PlayerStats
+    }
 
 
+    public void showCurrentPlayerLevel()
+    {
+        if (currentPlayerView != null)
+        {
+            //show current player and selected level
+            if (currentplayer == "" || currentplayer == null)
+                currentPlayerView.text = "PLEASE SELECT PLAYER";
+            else
+                currentPlayerView.text = "PLAYER: " + currentplayer + "\nLEVEL: " + selectedLevel;
+        }
+    }
+
+    public void populateLevelDropdown()
+    {
+        levelDropdown.ClearOptions();
+
+        if (currentplayer != "")
+        {
+            //add possible levels for player to dropdown
+            int levelReached = PlayerStats.playerData.players[currentplayer];
+            for (int i = 1; i <= levelReached; i++)
+            {
+                levelDropdown.options.Add(new Dropdown.OptionData() {text = "LEVEL " + (i)});
+            }
+
+            levelDropdown.RefreshShownValue();
+        }
+        else
+        {
+            levelDropdown.options.Add(new Dropdown.OptionData() {text = "CHOOSE PLAYER FIRST!"}); //no player chosen
+            levelDropdown.RefreshShownValue();
+        }
+
+        levelDropdown.value = 0;
+    }
+
+    public void populatePlayerDropdown()
+    {
+        playerDropdown.ClearOptions();
+
+        foreach (string currentKey in PlayerStats.playerData.players.Keys)
+        {
+            //get all players from Dictionary and put in dropdown
+            playerDropdown.options.Add(new Dropdown.OptionData() {text = currentKey});
+        }
+
+        playerDropdown.value = playerDropdown.options.Capacity; //show last player created
+    }
+
+    public void changeSelectedLevel()
+    {
+        levelDropdown.RefreshShownValue();
+        selectedLevel = levelDropdown.value + 1;
+    }
 }
